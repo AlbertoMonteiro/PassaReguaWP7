515791ebe3b841ca60b671509d3d442393d47224
Alberto Monteiro <betimbrasil@gmail.com>
2012-01-04 22:47:32 -0300

    adding conta view model

--- PassaRegua/Model/Conta.cs
+++ PassaRegua/Model/Conta.cs
@@ -1,20 +1,77 @@
-﻿namespace PassaRegua.Model
-{
-    public class Conta
-    {
-        public int PessoasSemBeber { get; set; }
+﻿
+using System.ComponentModel;
 
-        public int PessoasQueBeberam { get; set; }
+namespace PassaRegua.Model
+{
+    public class Conta : INotifyPropertyChanged
+    {
+        private int _pessoasSemBeber;
+        public int PessoasSemBeber
+        {
+            get { return _pessoasSemBeber; }
+            set
+            {
+                _pessoasSemBeber = value;
+                OnPropertyChanged("PessoasSemBeber");
+            }
+        }
+
+        private int _pessoasQueBeberam;
+        public int PessoasQueBeberam
+        {
+            get { return _pessoasQueBeberam; }
+            set
+            {
+                _pessoasQueBeberam = value;
+                OnPropertyChanged("PessoasQueBeberam");
+            }
+        }
 
         private int TotalDePessoas { get { return PessoasQueBeberam + PessoasSemBeber; } }
 
-        public decimal ValorDaConta { get; set; }
+        private decimal _valorDaConta;
+        public decimal ValorDaConta
+        {
+            get { return _valorDaConta; }
+            set
+            {
+                _valorDaConta = value;
+                OnPropertyChanged("ValorDaConta");
+            }
+        }
 
-        public decimal ValorEmBebida { get; set; }
+        private decimal _valorEmBebida;
+        public decimal ValorEmBebida
+        {
+            get { return _valorEmBebida; }
+            set
+            {
+                _valorEmBebida = value;
+                OnPropertyChanged("ValorEmBebida");
+            }
+        }
 
-        public decimal ValorDoServico { get; set; }
+        private decimal _valorDoServico;
+        public decimal ValorDoServico
+        {
+            get { return _valorDoServico; }
+            set
+            {
+                _valorDoServico = value;
+                OnPropertyChanged("ValorDoServico");
+            }
+        }
 
-        public decimal ValorExtra { get; set; }
+        private decimal _valorExtra;
+        public decimal ValorExtra
+        {
+            get { return _valorExtra; }
+            set
+            {
+                _valorExtra = value;
+                OnPropertyChanged("ValorExtra");
+            }
+        }
 
         private decimal ValorTotal { get { return ValorDaConta + ValorExtra + (ValorExtra * ValorDoServico / 100); } }
 
@@ -23,5 +80,14 @@
         public decimal ValorPorPessoaSemBeber { get { return TotalSemBebida / TotalDePessoas; } }
 
         public decimal ValorPorPessoaQueBebeu { get { return ValorEmBebida / PessoasQueBeberam + ValorPorPessoaSemBeber; } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
     }
 }
+
---
+++ PassaRegua/ViewModel/ContaViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using GalaSoft.MvvmLight;
+using PassaRegua.Model;
+
+namespace PassaRegua.ViewModel
+{
+    public class ContaViewModel : ViewModelBase
+    {
+        public Conta Model { get; set; }
+
+        public ContaViewModel(Conta conta)
+        {
+            Model = conta;
+        }
+    }
+}
